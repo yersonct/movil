3cddeeb3bed3e15b97c5e967e4a8e80cb66ccb10
nataliosorio <nataliaosorio973@gmail.com>
2025-07-22 17:33:23 -0500

    template-ionic

---
+++ ANPR-APP/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/inbox',
+    pathMatch: 'full',
+  },
+  {
+    path: 'folder/:id',
+    loadComponent: () =>
+      import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'pages/dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+
+
+];
