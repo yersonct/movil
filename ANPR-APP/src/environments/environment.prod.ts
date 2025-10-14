3d898baabd7b5910a3b0423593c2cbb613507253
nataliosorio <nataliaosorio973@gmail.com>
2025-10-14 09:35:29 -0500

    server

--- ANPR-APP/src/environments/environment.prod.ts
+++ ANPR-APP/src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
+// export const environment = {
+//    production: false,
+//   development: true,
+//     apiURL: "https://localhost:7159/api",
+// };
+
+
+
+
 export const environment = {
-   production: false,
-  development: true,
-    apiURL: "https://localhost:7159/api",
+  production: true,
+  apiURL: "https://anpr-vision-gwejhpfyf9exhvax.brazilsouth-01.azurewebsites.net/api"
 };
--- ANPR-APP/src/environments/environment.ts
+++ ANPR-APP/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  production: false,
+  apiURL: "https://localhost:7159/api"
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
